012a24cca5724c269a4791d32d9d2ede22b3dea2
Bharath <39058992+Bharath9t7@users.noreply.github.com>
2023-03-25 22:05:42 +0530

    few updates (#15)

--- modules/raw/sales/customer/file_formats.tf
+++ modules/raw/sales/customer/file_formats.tf
@@ -1,59 +1,59 @@
 # file formats to load data from stages
 
 resource "snowflake_file_format" "file_format_pipe" {
-  database         = var.target_db_name
-  schema           = var.target_schema_name
-  name             = "SNOWFLAKE_LOAD_FF_PIPE"
-  format_type      = "CSV"
-  field_delimiter  = "|"
-  compression      = "AUTO"
-  record_delimiter = "\n"
-  binary_format    = "UTF-8"
-  skip_header      = "0"
-  escape = "NONE"
-  time_format = "AUTO"
-  timestamp_format = "AUTO"
-  date_format = "AUTO"
-  encoding = "UTF8"
-  escape_unenclosed_field = "NONE"
+  database                     = var.target_db_name
+  schema                       = var.target_schema_name
+  name                         = "SNOWFLAKE_LOAD_FF_PIPE"
+  format_type                  = "CSV"
+  field_delimiter              = "|"
+  compression                  = "AUTO"
+  record_delimiter             = "\n"
+  binary_format                = "UTF-8"
+  skip_header                  = "0"
+  escape                       = "NONE"
+  time_format                  = "AUTO"
+  timestamp_format             = "AUTO"
+  date_format                  = "AUTO"
+  encoding                     = "UTF8"
+  escape_unenclosed_field      = "NONE"
   field_optionally_enclosed_by = "NONE"
 }
 
 resource "snowflake_file_format" "file_format_comma" {
-  database         = var.target_db_name
-  schema           = var.target_schema_name
-  name             = "SNOWFLAKE_LOAD_FF_COMMA"
-  format_type      = "CSV"
-  field_delimiter  = ","
-  compression      = "AUTO"
-  record_delimiter = "\n"
-  binary_format    = "UTF-8"
-  skip_header      = "0"
-  escape = "NONE"
-  time_format = "AUTO"
-  timestamp_format = "AUTO"
-  date_format = "AUTO"
-  encoding = "UTF8"
-  escape_unenclosed_field = "NONE"
+  database                     = var.target_db_name
+  schema                       = var.target_schema_name
+  name                         = "SNOWFLAKE_LOAD_FF_COMMA"
+  format_type                  = "CSV"
+  field_delimiter              = ","
+  compression                  = "AUTO"
+  record_delimiter             = "\n"
+  binary_format                = "UTF-8"
+  skip_header                  = "0"
+  escape                       = "NONE"
+  time_format                  = "AUTO"
+  timestamp_format             = "AUTO"
+  date_format                  = "AUTO"
+  encoding                     = "UTF8"
+  escape_unenclosed_field      = "NONE"
   field_optionally_enclosed_by = "NONE"
 }
 
 resource "snowflake_file_format" "file_format_pipe2" {
-  database         = var.target_db_name
-  schema           = var.target_schema_name
-  name             = "SNOWFLAKE_LOAD_FF_PIPE2"
-  format_type      = "CSV"
-  field_delimiter  = "|"
-  compression      = "AUTO"
-  record_delimiter = "\n"
-  binary_format    = "UTF-8"
-  skip_header      = "0"
-  escape = "NONE"
-  time_format = "AUTO"
-  timestamp_format = "AUTO"
-  date_format = "AUTO"
-  encoding = "UTF8"
-  escape_unenclosed_field = "NONE"
+  database                     = var.target_db_name
+  schema                       = var.target_schema_name
+  name                         = "SNOWFLAKE_LOAD_FF_PIPE2"
+  format_type                  = "CSV"
+  field_delimiter              = "|"
+  compression                  = "AUTO"
+  record_delimiter             = "\n"
+  binary_format                = "UTF-8"
+  skip_header                  = "0"
+  escape                       = "NONE"
+  time_format                  = "AUTO"
+  timestamp_format             = "AUTO"
+  date_format                  = "AUTO"
+  encoding                     = "UTF8"
+  escape_unenclosed_field      = "NONE"
   field_optionally_enclosed_by = "NONE"
 }
 
