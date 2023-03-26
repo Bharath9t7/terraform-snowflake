902f105b0db0428a4892e2228478e6307bd0b25d
Bharath <39058992+Bharath9t7@users.noreply.github.com>
2023-03-26 23:01:08 +0530

    Dev (#22)
    * few updates
    
    * few updates
    
    * Update file_formats.tf
    
    * Update snowflake-terraform-dev.yml
    
    * Update snowflake-terraform-main.yml
    
    * few updates
    
    * Update file_formats.tf
    
    * updates
    
    * Update file_formats.tf
    
    * Update snowflake-terraform-dev.yml
    
    * Update file_formats.tf
    
    * update workflows
    
    * ..
    
    * Update file_formats.tf

--- modules/raw/sales/customer/file_formats.tf
+++ modules/raw/sales/customer/file_formats.tf
@@ -56,26 +56,6 @@ resource "snowflake_file_format" "file_format_pipe2" {
   escape_unenclosed_field      = "NONE"
   field_optionally_enclosed_by = "NONE"
 }
-
-resource "snowflake_file_format" "file_format_comma2" {
-  database                     = var.target_db_name
-  schema                       = var.target_schema_name
-  name                         = "SNOWFLAKE_LOAD_FF_COMMA4"
-  format_type                  = "CSV"
-  field_delimiter              = ","
-  compression                  = "AUTO"
-  record_delimiter             = "\n"
-  binary_format                = "UTF-8"
-  skip_header                  = "0"
-  escape                       = "NONE"
-  time_format                  = "AUTO"
-  timestamp_format             = "AUTO"
-  date_format                  = "AUTO"
-  encoding                     = "UTF8"
-  escape_unenclosed_field      = "NONE"
-  field_optionally_enclosed_by = "NONE"
-}
-
 # resource "snowflake_file_format_grant" "file_format_ownership_grant_1" {
 #   database_name     = var.target_db_name
 #   schema_name       = var.target_schema_name
